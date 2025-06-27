9a6e415808ee526065d4a22b641731707c74ded6
wenyinh <wenyinh@andrew.cmu.edu>
2025-06-28 00:33:02 +0800

    实现登录拦截器

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
@@ -64,8 +66,9 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             // 5. 不存在，注册逻辑
             user = createUserWithPhone(phone);
         }
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
         // 6. 写用户session
-        session.setAttribute("user", user);
+        session.setAttribute("user", userDTO);
         return Result.ok();
     }
 
---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,29 @@
+package com.hmdp.utils;
+
+import com.hmdp.dto.UserDTO;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        Object userDTO = session.getAttribute("user");
+        if (userDTO == null) {
+            response.setStatus(401);
+            return false;
+        }
+        UserHolder.saveUser((UserDTO) userDTO);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        UserHolder.removeUser();
+    }
+}
